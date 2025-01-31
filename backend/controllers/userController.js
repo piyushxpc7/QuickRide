df5eaab64a44163b2d3663bf31c0b18ccab7a18a
Caleb <justcaleb01@gmail.com>
2025-01-31 14:45:46 +0100

    created function  to create and sign user token and store them in the browser cookies using http

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,18 @@
 const User = require("../models/userModels");
+const createSignToken = require("../utils/createSignToken");
 
 exports.register = async (req, res, next) => {
+  const { username, email, password } = req.body;
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({ username, email, password });
+    //  Create a token for the user
+    const token = createSignToken(newUser._id);
+    // Send the token to the user in a cookie
+    res.cookie("token", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
+      secure: true,
+      httpOnly: true,
+    });
 
     res.status(201).json({
       status: "success",
